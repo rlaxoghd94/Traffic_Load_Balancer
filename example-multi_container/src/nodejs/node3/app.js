e7ff8e5489bfc38627000dc4edf374027d1ccd42
Nic Taehong Kim <ktae.nic@gmail.com>
2019-01-24 05:10:23 -0800

    example-multi_container Upload

---
+++ example-multi_container/src/nodejs/node3/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+   res.send('<h1>Working on Node 3</h1>');
+});
+
+app.get('/testtest', (req, res) => {
+   res.send("You've entered the test page");
+});
+
+app.listen(8082, '127.0.0.1');
+
+console.log('Server running at 127.0.0.1:8082');
