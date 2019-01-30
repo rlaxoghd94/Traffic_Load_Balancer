ac43c562893c7d3694901cef003c3e540ae29ad4
Nic Taehong Kim <ktae.nic@gmail.com>
2019-01-29 23:35:01 -0800

    issue-node_merge fixed

--- example-multi_container/src/nodejs/app.js
+++ example-multi_container/src/nodejs/app.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-   res.send('<h1>Working on Node 2</h1>');
+   res.send('<h1>Working on Node' + process.pid + '</h1>');
 });
 
 app.get('/testtest', (req, res) => {
    res.send("You've entered the test page");
 });
 
-app.listen(8081, '0.0.0.0');
+app.listen(8080, '0.0.0.0');
 
-console.log('Server running at 127.0.0.1:8081');
+console.log('Server running at 127.0.0.1:8080');
